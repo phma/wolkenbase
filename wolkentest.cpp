e6eda9cd021b80caa4e7559063ea3b497913c03c
Pierre Abbat <phma@bezitopo.org>
2019-09-06 08:29:31 +0000

    Add a test program with no tests yet

---
+++ wolkentest.cpp
@@ -0,0 +1,74 @@
+/******************************************************/
+/*                                                    */
+/* wolkentest.cpp - test program                      */
+/*                                                    */
+/******************************************************/
+/* Copyright 2019 Pierre Abbat.
+ * This file is part of Wolkenbase.
+ *
+ * Wolkenbase is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Wolkenbase is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Wolkenbase. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <algorithm>
+#include <cstdlib>
+#include <csignal>
+#include <cfloat>
+#include <cstring>
+#include <unistd.h>
+#include <vector>
+#include <string>
+
+#define tassert(x) testfail|=(!(x))
+//#define tassert(x) if (!(x)) {testfail=true; sleep(10);}
+// so that tests still work in non-debug builds
+
+using namespace std;
+
+bool testfail=false;
+vector<string> args;
+
+bool shoulddo(string testname)
+{
+  int i;
+  bool ret,listTests=false;
+  if (testfail)
+  {
+    cout<<"failed before "<<testname<<endl;
+    //sleep(2);
+  }
+  ret=args.size()==0;
+  for (i=0;i<args.size();i++)
+  {
+    if (testname==args[i])
+      ret=true;
+    if (args[i]=="-l")
+      listTests=true;
+  }
+  if (listTests)
+    cout<<testname<<endl;
+  return ret;
+}
+
+int main(int argc, char *argv[])
+{
+  int i;
+  for (i=1;i<argc;i++)
+    args.push_back(argv[i]);
+  cout<<"\nTest "<<(testfail?"failed":"passed")<<endl;
+  return testfail;
+}
