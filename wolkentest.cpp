d77c49e7abc1021533db76d1eff33655d23de372
Pierre Abbat <phma@bezitopo.org>
2020-11-08 08:04:58 +0000

    Add the data for a well-known text test

--- wolkentest.cpp
+++ wolkentest.cpp
@@ -270,6 +270,24 @@ void testsplitfile()
   test1splitfile(5779,5762);
 }
 
+void testwkt()
+{
+  char wvawkt[]= // Well-known text from square of West Virginia terrain
+  "COMPD_CS[\"NAD83 / UTM zone 17N + NAVD88 height\",PROJCS[\"NAD83 / UTM zone 17N\","
+  "GEOGCS[\"NAD83\",DATUM[\"North_American_Datum_1983\",SPHEROID[\"GRS 1980\","
+  "6378137,298.257222101,AUTHORITY[\"EPSG\",\"7019\"]],TOWGS84[0,0,0,0,0,0,0],"
+  "AUTHORITY[\"EPSG\",\"6269\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],"
+  "UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9122\"]],AUTHORITY"
+  "[\"EPSG\",\"4269\"]],PROJECTION[\"Transverse_Mercator\"],PARAMETER"
+  "[\"latitude_of_origin\",0],PARAMETER[\"central_meridian\",-81],PARAMETER"
+  "[\"scale_factor\",0.9996],PARAMETER[\"false_easting\",500000],PARAMETER"
+  "[\"false_northing\",0],UNIT[\"meter\",1,AUTHORITY[\"EPSG\",\"9001\"]],"
+  "AXIS[\"X\",EAST],AXIS[\"Y\",NORTH],AUTHORITY[\"EPSG\",\"26917\"]],VERT_CS"
+  "[\"NAVD88 height\",VERT_DATUM[\"North American Vertical Datum 1988\",2005,"
+  "AUTHORITY[\"EPSG\",\"5103\"]],UNIT[\"meter\",1,AUTHORITY[\"EPSG\",\"9001\"]],"
+  "AXIS[\"Up\",UP],AUTHORITY[\"EPSG\",\"5703\"]]]";
+}
+
 bool shoulddo(string testname)
 {
   int i;
@@ -316,6 +334,8 @@ int main(int argc, char *argv[])
     testflowsnake();
   if (shoulddo("splitfile"))
     testsplitfile();
+  if (shoulddo("wkt"))
+    testwkt();
   cout<<"\nTest "<<(testfail?"failed":"passed")<<endl;
   return testfail;
 }
