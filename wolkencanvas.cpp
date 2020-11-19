312ecb0ec905eb0754e27950cc612bb8a94afade
Pierre Abbat <phma@bezitopo.org>
2020-11-19 07:44:42 +0000

    Add a canvas class

---
+++ wolkencanvas.cpp
@@ -0,0 +1,322 @@
+/******************************************************/
+/*                                                    */
+/* wolkencanvas.cpp - canvas for drawing point cloud  */
+/*                                                    */
+/******************************************************/
+/* Copyright 2020 Pierre Abbat.
+ * This file is part of Wolkenbase.
+ *
+ * Wolkenbase is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Wolkenbase is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Wolkenbase. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <QPainter>
+#include <cmath>
+#include "wolkencanvas.h"
+#include "boundrect.h"
+#include "relprime.h"
+#include "angle.h"
+#include "threads.h"
+#include "ldecimal.h"
+
+using namespace std;
+namespace cr=std::chrono;
+
+QPoint qptrnd(xy pnt)
+{
+  return QPoint(lrint(pnt.getx()),lrint(pnt.gety()));
+}
+
+WolkenCanvas::WolkenCanvas(QWidget *parent):QWidget(parent)
+{
+  setAutoFillBackground(true);
+  setBackgroundRole(QPalette::Base);
+  setMinimumSize(40,30);
+  triangleNum=splashScreenTime=dartAngle=ballAngle=0;
+}
+
+QPointF WolkenCanvas::worldToWindow(xy pnt)
+{
+  pnt.roscat(worldCenter,0,scale,windowCenter);
+  QPointF ret(pnt.getx(),height()-pnt.gety());
+  return ret;
+}
+
+xy WolkenCanvas::windowToWorld(QPointF pnt)
+{
+  xy ret(pnt.x(),height()-pnt.y());
+  ret.roscat(windowCenter,0,1/scale,worldCenter);
+  return ret;
+}
+
+void WolkenCanvas::sizeToFit()
+{
+  int i;
+  double xscale,yscale;
+  BoundRect br;
+  if (br.left()>=br.right() && br.bottom()>=br.top())
+  {
+    worldCenter=xy(0,0);
+    scale=1;
+  }
+  else
+  {
+    worldCenter=xy((br.left()+br.right())/2,(br.bottom()+br.top())/2);
+    xscale=width()/(br.right()-br.left());
+    yscale=height()/(br.top()-br.bottom());
+    if (xscale<yscale)
+      scale=xscale;
+    else
+      scale=yscale;
+  }
+  if (splashScreenTime)
+    scale=scale*2/3;
+}
+
+void WolkenCanvas::tick()
+{
+  int i,sz,timeLimit;
+  int tstatus=getThreadStatus();
+  double splashElev;
+  xy gradient,A,B,C;
+  xy dartCorners[4];
+  xy leftTickmark,rightTickmark,tickmark;
+  QPolygonF polygon;
+  QPolygon swath;
+  QPointF circleCenter;
+  QRectF circleBox,textBox;
+  string scaleText;
+  cr::nanoseconds elapsed=cr::milliseconds(0);
+  cr::time_point<cr::steady_clock> timeStart=clk.now();
+  xy oldBallPos=ballPos;
+  QPainter painter(&frameBuffer);
+  QBrush brush;
+  QPen pen;
+  painter.setRenderHint(QPainter::Antialiasing,true);
+  painter.setPen(Qt::NoPen);
+  brush.setStyle(Qt::SolidPattern);
+  if ((tstatus&0x3ffbfeff)%1048577==0)
+    state=(tstatus&0x3ffbfeff)/1048577;
+  else
+  {
+    state=0;
+    //cout<<"tstatus "<<((tstatus>>20)&1023)<<':'<<((tstatus>>10)&1023)<<':'<<(tstatus&1023)<<endl;
+  }
+  if ((state==TH_WAIT || state==TH_PAUSE) && currentAction)
+    state=-currentAction;
+  // Compute the new position of the ball, and update a swath containing the ball's motion.
+  penPos=(penPos+5)%144;
+  ballPos=lis.move()+xy(10,10);
+  switch ((ballPos.gety()>oldBallPos.gety())*2+(ballPos.getx()>oldBallPos.getx()))
+  {
+    case 0: // Ball is moving up and left
+      swath<<qptrnd(ballPos+xy(-10.5,-10.5))<<qptrnd(ballPos+xy(-10.5,10.5));
+      swath<<qptrnd(oldBallPos+xy(-10.5,10.5))<<qptrnd(oldBallPos+xy(10.5,10.5));
+      swath<<qptrnd(oldBallPos+xy(10.5,-10.5))<<qptrnd(ballPos+xy(10.5,-10.5));
+      break;
+    case 1: // Ball is moving up and right
+      swath<<qptrnd(oldBallPos+xy(-10.5,-10.5))<<qptrnd(oldBallPos+xy(-10.5,10.5));
+      swath<<qptrnd(oldBallPos+xy(10.5,10.5))<<qptrnd(ballPos+xy(10.5,10.5));
+      swath<<qptrnd(ballPos+xy(10.5,-10.5))<<qptrnd(ballPos+xy(-10.5,-10.5));
+      break;
+    case 2: // Ball is moving down and left
+      swath<<qptrnd(oldBallPos+xy(10.5,10.5))<<qptrnd(oldBallPos+xy(10.5,-10.5));
+      swath<<qptrnd(oldBallPos+xy(-10.5,-10.5))<<qptrnd(ballPos+xy(-10.5,-10.5));
+      swath<<qptrnd(ballPos+xy(-10.5,10.5))<<qptrnd(ballPos+xy(10.5,10.5));
+      break;
+    case 3: // Ball is moving down and right
+      swath<<qptrnd(ballPos+xy(10.5,10.5))<<qptrnd(ballPos+xy(10.5,-10.5));
+      swath<<qptrnd(oldBallPos+xy(10.5,-10.5))<<qptrnd(oldBallPos+xy(-10.5,-10.5));
+      swath<<qptrnd(oldBallPos+xy(-10.5,10.5))<<qptrnd(ballPos+xy(-10.5,10.5));
+      break;
+  }
+  update(QRegion(swath));
+  // Paint a dart (concave quadrilateral) outside the TIN white.
+  dartAngle+=PHITURN;
+  dartCorners[0]=hypot(width(),height())*0.51/scale*cossin(dartAngle)+worldCenter;
+  for (i=0;i<4;i++)
+    polygon<<worldToWindow(dartCorners[i]);
+  painter.setBrush(Qt::white);
+  painter.drawPolygon(polygon);
+  // Paint a circle white as a background for the scale.
+  if (maxScaleSize>0)
+  {
+    circleCenter=worldToWindow(scaleEnd);
+    circleBox=QRectF(circleCenter.x()-maxScaleSize*scale,circleCenter.y()-maxScaleSize*scale,
+		    maxScaleSize*scale*2,maxScaleSize*scale*2);
+    painter.drawEllipse(circleBox);
+    // Draw the scale.
+    painter.setPen(Qt::black);
+    tickmark=turn90(rightScaleEnd-leftScaleEnd)/10;
+    leftTickmark=leftScaleEnd+tickmark;
+    rightTickmark=rightScaleEnd+tickmark;
+    polygon=QPolygonF();
+    polygon<<worldToWindow(leftTickmark);
+    polygon<<worldToWindow(leftScaleEnd);
+    polygon<<worldToWindow(rightScaleEnd);
+    polygon<<worldToWindow(rightTickmark);
+    painter.drawPolyline(polygon);
+    // Write the length of the scale.
+    textBox=QRectF(worldToWindow(leftScaleEnd+2*tickmark),worldToWindow(rightScaleEnd));
+    scaleText=ldecimal(scaleSize/lengthUnit,scaleSize/lengthUnit/10)+((lengthUnit==1)?" m":" ft");
+    painter.drawText(textBox,Qt::AlignCenter,QString::fromStdString(scaleText));
+  }
+  painter.setPen(Qt::NoPen);
+  // Paint some triangles in the TIN in colors depending on their gradient or elevation.
+  if (state==TH_WAIT || state==TH_PAUSE)
+    timeLimit=45;
+  else
+    timeLimit=20;
+}
+
+void WolkenCanvas::setSize()
+{
+  double oldScale=scale;
+  xy oldWindowCenter=windowCenter;
+  int dx,dy;
+  QTransform tf;
+  windowCenter=xy(width(),height())/2.;
+  lis.resize(width()-20,height()-20);
+  sizeToFit();
+  if (frameBuffer.isNull())
+  {
+    frameBuffer=QPixmap(width(),height());
+    frameBuffer.fill(Qt::white);
+  }
+  else
+  {
+    tf.translate(-oldWindowCenter.getx(),-oldWindowCenter.gety());
+    tf.scale(scale/oldScale,scale/oldScale);
+    tf.translate(windowCenter.getx(),windowCenter.gety());
+    QPixmap frameTemp=frameBuffer.transformed(tf);
+    frameBuffer=QPixmap(width(),height());
+    frameBuffer.fill(Qt::white);
+    QPainter painter(&frameBuffer);
+    dx=(width()-frameTemp.width())/2;
+    dy=(height()-frameTemp.height())/2;
+    QRect rect0(0,0,frameTemp.width(),frameTemp.height());
+    QRect rect1(dx,dy,frameTemp.width(),frameTemp.height());
+    painter.drawPixmap(rect1,frameTemp,rect0);
+    update();
+  }
+  setScalePos();
+}
+
+void WolkenCanvas::setScalePos()
+// Set the position of the scale at the lower left or right corner.
+{
+  xy leftScalePos,rightScalePos,disp;
+  bool right=false;
+  leftScalePos=windowToWorld(QPointF(width()*0.01,height()*0.99));
+  rightScalePos=windowToWorld(QPointF(width()*0.99,height()*0.99));
+  scaleSize=lengthUnit;
+  while (scaleSize>maxScaleSize)
+    scaleSize/=10;
+  while (scaleSize<maxScaleSize)
+    scaleSize*=10;
+  if (scaleSize/2<maxScaleSize)
+    scaleSize/=2;
+  else if (scaleSize/5<maxScaleSize)
+    scaleSize/=5;
+  else
+    scaleSize/=10;
+  if (right)
+  {
+    scaleEnd=rightScaleEnd=rightScalePos;
+    disp=leftScalePos-rightScalePos;
+    disp/=disp.length();
+    leftScaleEnd=rightScaleEnd+scaleSize*disp;
+  }
+  else
+  {
+    scaleEnd=leftScaleEnd=leftScalePos;
+    disp=rightScalePos-leftScalePos;
+    disp/=disp.length();
+    rightScaleEnd=leftScaleEnd+scaleSize*disp;
+  }
+}
+
+void WolkenCanvas::setLengthUnit(double unit)
+{
+  lengthUnit=unit;
+  setScalePos();
+}
+
+void WolkenCanvas::paintEvent(QPaintEvent *event)
+{
+  int i;
+  QPainter painter(this);
+  QRectF square(ballPos.getx()-10,ballPos.gety()-10,20,20);
+  QRectF sclera(ballPos.getx()-10,ballPos.gety()-5,20,10);
+  QRectF iris(ballPos.getx()-5,ballPos.gety()-5,10,10);
+  QRectF pupil(ballPos.getx()-2,ballPos.gety()-2,4,4);
+  QRectF paper(ballPos.getx()-7.07,ballPos.gety()-10,14.14,20);
+  QPolygonF octagon;
+  double x0,x1,y;
+  octagon<<QPointF(ballPos.getx()-10,ballPos.gety()-4.14);
+  octagon<<QPointF(ballPos.getx()-4.14,ballPos.gety()-10);
+  octagon<<QPointF(ballPos.getx()+4.14,ballPos.gety()-10);
+  octagon<<QPointF(ballPos.getx()+10,ballPos.gety()-4.14);
+  octagon<<QPointF(ballPos.getx()+10,ballPos.gety()+4.14);
+  octagon<<QPointF(ballPos.getx()+4.14,ballPos.gety()+10);
+  octagon<<QPointF(ballPos.getx()-4.14,ballPos.gety()+10);
+  octagon<<QPointF(ballPos.getx()-10,ballPos.gety()+4.14);
+  painter.setRenderHint(QPainter::Antialiasing,true);
+  painter.drawPixmap(this->rect(),frameBuffer,this->rect());
+  switch (state)
+  {
+    case TH_RUN:
+      painter.setBrush(Qt::yellow);
+      painter.setPen(Qt::NoPen);
+      painter.drawChord(square,lrint(bintodeg(ballAngle)*16),2880);
+      painter.setBrush(Qt::blue);
+      painter.drawChord(square,lrint(bintodeg(ballAngle+DEG180)*16),2880);
+      break;
+    case 99: // FIXME writing file
+      painter.setBrush(Qt::white);
+      painter.setPen(Qt::NoPen);
+      painter.drawRect(paper);
+      painter.setPen(Qt::black);
+      for (i=0;i<8;i++)
+      {
+	y=ballPos.gety()-9+i*18./7;
+	x0=ballPos.getx()-6;
+	if (penPos/18==i)
+	  x1=x0+(i%18+0.5)*2/3;
+	else
+	  x1=x0+12*(i<penPos/18);
+	if (x1>x0)
+	  painter.drawLine(QPointF(x0,y),QPointF(x1,y));
+      }
+      break;
+     case -ACT_LOAD:
+      painter.setPen(Qt::NoPen);
+      painter.setBrush(Qt::lightGray);
+      painter.drawEllipse(sclera);
+      painter.setBrush(Qt::blue);
+      painter.drawEllipse(iris);
+      painter.setBrush(Qt::black);
+      painter.drawEllipse(pupil);
+      break;
+    case 0:
+      painter.setBrush(Qt::lightGray);
+      painter.setPen(Qt::NoPen);
+      painter.drawEllipse(QPointF(ballPos.getx(),ballPos.gety()),10,10);
+      break;
+  }
+}
+
+void WolkenCanvas::resizeEvent(QResizeEvent *event)
+{
+  setSize();
+  QWidget::resizeEvent(event);
+}
---
+++ wolkencanvas.h
@@ -0,0 +1,66 @@
+/******************************************************/
+/*                                                    */
+/* wolkencanvas.h - canvas for drawing point cloud    */
+/*                                                    */
+/******************************************************/
+/* Copyright 2020 Pierre Abbat.
+ * This file is part of Wolkenbase.
+ *
+ * Wolkenbase is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Wolkenbase is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Wolkenbase. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef WOLKENCANVAS_H
+#define WOLKENCANVAS_H
+#include <QWidget>
+#include "lissajous.h"
+#include "point.h"
+
+#define SPLASH_TIME 60
+
+class WolkenCanvas: public QWidget
+{
+  Q_OBJECT
+public:
+  WolkenCanvas(QWidget *parent=0);
+  QPointF worldToWindow(xy pnt);
+  xy windowToWorld(QPointF pnt);
+  int state;
+signals:
+  void splashScreenStarted();
+  void splashScreenFinished();
+public slots:
+  void sizeToFit();
+  void setSize();
+  void setLengthUnit(double unit);
+  void setScalePos();
+  void tick(); // 50 ms
+protected:
+  void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
+  void resizeEvent(QResizeEvent *event) Q_DECL_OVERRIDE;
+private:
+  QPixmap frameBuffer;
+  Lissajous lis;
+  xy windowCenter,worldCenter;
+  double scale;
+  double lengthUnit;
+  double maxScaleSize,scaleSize;
+  xy ballPos;
+  xy leftScaleEnd,rightScaleEnd,scaleEnd;
+  int penPos;
+  int triangleNum;
+  int lastOpcount;
+  int ballAngle,dartAngle;
+  int lastntri;
+  int splashScreenTime; // in ticks
+};
+#endif
